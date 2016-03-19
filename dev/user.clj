bea66e0177a1f62a83cd45ea59f7af11bd347345
djjolicoeur <djjolicoeur@gmail.com>
2016-03-19 09:54:53 -0400

    save progress

--- dev/user.clj
+++ dev/user.clj
@@ -1,9 +1,10 @@
 (ns user
   (:require [clojure.tools.namespace.repl :refer [refresh]]
             [com.stuartsierra.component :as component]
+            [datomic.api :as d]
+            [datomic-talk.system :as app]
             [reloaded.repl
-             :refer [go init reset reset-all start stop system]]
-            [datomic-talk.system :as app]))
+             :refer [go init reset reset-all start stop system]]))
 
 
 (reloaded.repl/set-init! app/dev-system)
